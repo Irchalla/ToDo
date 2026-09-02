69614f6ae006f4e3a5eee43e130101d7c454d857
Irchalla <h.hatuev.020@gmail.com>
2026-09-02 21:59:16 +0300

    Initial Commit

---
+++ empty/emptyApp.swift
@@ -0,0 +1,17 @@
+//
+//  emptyApp.swift
+//  empty
+//
+//  Created by Khassan Khatuewv on 02.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct emptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
